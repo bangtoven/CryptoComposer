10617e7bcb337ef9225fad690870212d5e7ed46f
Jungho Bang <me@bangtoven.com>
2021-11-28 19:02:47 -0800

    vercel

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -7,9 +7,9 @@ module.exports = {
     const config = opts.webpackConfig;
 
     // Ignore fs dependencies so we can use winston
-    // if (opts.env.target === 'node' && !opts.env.dev) {
-    config.node = { fs: 'empty' };
-    // }
+    if (opts.env.target === 'node' && !opts.env.dev) {
+      config.node = { fs: 'empty' };
+    }
 
     return config;
   },
