fa39cc82e6af6301d721c6146706a95fca68f842
Jungho Bang <me@bangtoven.com>
2021-12-31 22:28:07 -0800

    add minter role

--- contracts/CryptoComposer.sol
+++ contracts/CryptoComposer.sol
@@ -4,21 +4,25 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CryptoComposer is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+contract CryptoComposer is ERC721, ERC721Enumerable, ERC721Burnable, AccessControl {
     using Counters for Counters.Counter;
 
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("CryptoComposer", "CC") {}
+    constructor() ERC721("CryptoComposer", "CC") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "http://crypto-composer.bangtoven.com/";
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
@@ -36,7 +40,7 @@ contract CryptoComposer is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
