8f35b07d0719717d4417c50004f84a683fc6542c
Jungho Bang <me@bangtoven.com>
2022-01-12 14:01:42 -0800

    yay

--- razzle.config.js
+++ razzle.config.js
@@ -8,7 +8,9 @@ module.exports = {
 
     // Ignore fs dependencies so we can use winston
     if (opts.env.target === 'node' && !opts.env.dev) {
-      config.resolve.fallback.fs = false;
+      config.resolve.fallback = {
+        fs: false,
+      };
     }
 
     config.performance = {
