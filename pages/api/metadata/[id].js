4a91574cf70ec7d9762ef794f93c98cb92c8e97b
Jungho Bang <me@bangtoven.com>
2022-01-13 00:07:24 -0800

    metadata api

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -2,5 +2,10 @@
 
 export default function handler(req, res) {
   const { id } = req.query;
-  res.status(200).json({ asdf: `id: ${id}` });
+  res.status(200).json({
+    name: `Song #${id} on Crypto Composer`,
+    description: 'NFT representing a piece of music registered on blockchain',
+    external_url: `https://crypto-composer.bangtoven.com/songs/${id}`,
+    image: 'https://crypto-composer.bangtoven.com/hero.jpg',
+  });
 }
