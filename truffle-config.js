2477b405aed0b5c577ba35fc1e93bb499005a431
Jungho Bang <me@bangtoven.com>
2022-01-12 17:37:14 -0800

    prep for migration

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
   /**
