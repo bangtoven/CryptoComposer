07cef198dc9a5806dc099f6f015c584b111c0231
Jungho Bang <me@bangtoven.com>
2022-01-13 22:34:52 -0800

    layout

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ const Layout = (props) => {
       <Head>
         <title>Crypto Composer</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=900, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#000000" />
         <style>@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');</style>
       </Head>
