3e646070074c24065d0e3ccb6742680ac8835728
Jungho Bang <me@bangtoven.com>
2022-01-15 01:01:37 -0800

    footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,24 @@
+import Link from 'next/link';
+import theme from '../styles/theme';
 import Header from './Header';
 
 import css from './Layout.module.css';
+import Text from './Text';
 
 const Layout = (props) => {
   return (
-    <div>
+    <div className={css.container}>
       <Header />
       <div className={css.main}>{props.children}</div>
+      <div className={css.footer}>
+        <Link href={'https://github.com/bangtoven/crypto-composer'}>
+          <a>
+            <Text t4 color={theme.lightBlue}>
+              Copyright © 2022, Jungho Bang.
+            </Text>
+          </a>
+        </Link>
+      </div>
     </div>
   );
 };
