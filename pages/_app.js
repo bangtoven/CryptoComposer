9ba6a1ff551a44c9b1a6f2d73569072638f53956
Jungho Bang <me@bangtoven.com>
2022-01-14 22:12:16 -0800

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ import { AppContextProvider } from '../components/utils/AppContext';
 import { Web3ReactProvider } from '@web3-react/core';
 import { AlertContextProvider } from '../components/Alert';
 
+import Script from 'next/script';
+
 function getLibrary(provider) {
   return new ethers.providers.Web3Provider(provider);
 }
@@ -20,6 +22,17 @@ const MyApp = ({ Component, pageProps }) => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=0.1" />
       </Head>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-VCHYVKGEN6`} />
+      <Script strategy="lazyOnload" id="my-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VCHYVKGEN6', {
+              page_path: window.location.pathname,
+            });
+          `}
+      </Script>
 
       <AppContextProvider>
         <Web3ReactProvider getLibrary={getLibrary}>
